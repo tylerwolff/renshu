9cc74ad30ec084f2c3a36a90700459a9e40b17db
Tyler Wolff <tylerwolff@gmail.com>
2019-01-11 22:05:36 -0500

    add hiragana character set

---
+++ src/lib/hiragana.js
@@ -0,0 +1,59 @@
+import ROWS from "./rows";
+
+export default [
+  { romaji: "a", character: "あ", row: ROWS.A },
+  { romaji: "i", character: "い", row: ROWS.A },
+  { romaji: "u", character: "う", row: ROWS.A },
+  { romaji: "e", character: "え", row: ROWS.A },
+  { romaji: "o", character: "お", row: ROWS.A },
+
+  { romaji: "ka", character: "か", row: ROWS.KA },
+  { romaji: "ki", character: "き", row: ROWS.KA },
+  { romaji: "ku", character: "く", row: ROWS.KA },
+  { romaji: "ke", character: "け", row: ROWS.KA },
+  { romaji: "ko", character: "こ", row: ROWS.KA },
+
+  { romaji: "sa", character: "さ", row: ROWS.SA },
+  { romaji: "shi", character: "し", row: ROWS.SA },
+  { romaji: "su", character: "す", row: ROWS.SA },
+  { romaji: "se", character: "せ", row: ROWS.SA },
+  { romaji: "so", character: "そ", row: ROWS.SA },
+
+  { romaji: "ta", character: "た", row: ROWS.TA },
+  { romaji: "chi", character: "ち", row: ROWS.TA },
+  { romaji: "tsu", character: "つ", row: ROWS.TA },
+  { romaji: "te", character: "て", row: ROWS.TA },
+  { romaji: "to", character: "と", row: ROWS.TA },
+
+  { romaji: "na", character: "な", row: ROWS.NA },
+  { romaji: "ni", character: "に", row: ROWS.NA },
+  { romaji: "nu", character: "ぬ", row: ROWS.NA },
+  { romaji: "ne", character: "ね", row: ROWS.NA },
+  { romaji: "no", character: "の", row: ROWS.NA },
+
+  { romaji: "ha", character: "は", row: ROWS.HA },
+  { romaji: "hi", character: "ひ", row: ROWS.HA },
+  { romaji: "fu", character: "ふ", row: ROWS.HA },
+  { romaji: "he", character: "へ", row: ROWS.HA },
+  { romaji: "ho", character: "ほ", row: ROWS.HA },
+
+  { romaji: "ma", character: "ま", row: ROWS.MA },
+  { romaji: "mi", character: "み", row: ROWS.MA },
+  { romaji: "mu", character: "む", row: ROWS.MA },
+  { romaji: "me", character: "め", row: ROWS.MA },
+  { romaji: "mo", character: "も", row: ROWS.MA },
+
+  { romaji: "ra", character: "ら", row: ROWS.RA },
+  { romaji: "ri", character: "り", row: ROWS.RA },
+  { romaji: "ru", character: "る", row: ROWS.RA },
+  { romaji: "re", character: "れ", row: ROWS.RA },
+  { romaji: "ro", character: "ろ", row: ROWS.RA },
+
+  { romaji: "ya", character: "や", row: ROWS.YA },
+  { romaji: "yu", character: "ゆ", row: ROWS.YA },
+  { romaji: "yo", character: "よ", row: ROWS.YA },
+
+  { romaji: "wa", character: "わ", row: ROWS.WA },
+  { romaji: "wo", character: "を", row: ROWS.WA },
+  { romaji: "n", character: "ん", row: ROWS.WA }
+];
---
+++ src/lib/rows.js
@@ -0,0 +1,13 @@
+export default {
+  A: "a",
+  KA: "ka",
+  SA: "sa",
+  TA: "ta",
+  NA: "na",
+  HA: "ha",
+  MA: "ma",
+  YA: "ya",
+  RA: "ra",
+  WA: "wa",
+  ALT: "alt"
+};
