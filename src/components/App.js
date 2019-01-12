23cd965f5abd63ac75f4501e44a1e376a7c2950e
Tyler Wolff <tylerwolff@gmail.com>
2019-01-12 16:52:33 -0500

    add link back to site

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from "react";
+import styled from "@emotion/styled";
 import Quiz from "./Quiz";
+
+const AppInfo = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 1em;
+  font-size: 1em;
+`;
 
 class App extends Component {
   render() {
-    return <Quiz />;
+    return (
+      <div>
+        <Quiz />
+        <AppInfo>
+          Made by{" "}
+          <a
+            href="https://tylerwolff.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            tw
+          </a>
+        </AppInfo>
+      </div>
+    );
   }
 }
 
