ad91cd25c4f0852856c351d3bdb5fd2f7db188bb
Tyler Wolff <tylerwolff@gmail.com>
2019-01-20 15:53:11 -0500

    add numbers data file

---
+++ src/lib/numbers.js
@@ -0,0 +1,15 @@
+export default [
+  { character: '一', sound: '1', romaji: 'ichi' },
+  { character: '二', sound: '2', romaji: 'ni' },
+  { character: '三', sound: '3', romaji: 'san' },
+  { character: '四', sound: '4', romaji: 'yon' },
+  { character: '五', sound: '5', romaji: 'go' },
+  { character: '六', sound: '6', romaji: 'roku' },
+  { character: '七', sound: '7', romaji: 'nana' },
+  { character: '八', sound: '8', romaji: 'hachi' },
+  { character: '九', sound: '9', romaji: 'kyu' },
+  { character: '十', sound: '10', romaji: 'ju' },
+  { character: '百', sound: '100', romaji: 'hyaku' },
+  { character: '千', sound: '1000', romaji: 'sen' },
+  { character: '万', sound: '10000', romaji: 'ichiman' },
+];
