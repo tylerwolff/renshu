ad6068d3062697991328438079fe52fead4eb100
Tyler Wolff <tylerwolff@gmail.com>
2019-01-20 16:25:34 -0500

    add settings to kana quiz only

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,17 @@
 import React, { Component } from 'react';
-import styled from '@emotion/styled';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from 'react-router-dom';
-import Settings from './Settings';
 
 // Quizes
 import QuizKana from './QuizKana';
 import QuizNumbers from './QuizNumbers';
 
-import { ReactComponent as SettingsIcon } from '../assets/round-settings-24px.svg';
-
-const SettingsLink = styled.button`
-  position: absolute;
-  bottom: 1em;
-  left: 1em;
-  background: none;
-  border: 0;
-  opacity: 0.5;
-  cursor: pointer;
-  transition: opacity 0.2s ease-out;
-
-  span {
-    display: none;
-    padding-left: 0.4em;
-    vertical-align: middle;
-  }
-
-  &:hover {
-    opacity: 1;
-
-    span {
-      display: inline-block;
-    }
-  }
-`;
-
 class App extends Component {
-  state = {
-    showSettings: false,
-  };
-
   render() {
-    const { showSettings } = this.state;
-
     return (
       <div>
         <Router>
@@ -56,16 +21,6 @@ class App extends Component {
             <Redirect to="/" />
           </Switch>
         </Router>
-        <SettingsLink
-          type="button"
-          onClick={() => this.setState({ showSettings: true })}
-        >
-          <SettingsIcon />
-          <span>Settings</span>
-        </SettingsLink>
-        {showSettings && (
-          <Settings onClose={() => this.setState({ showSettings: false })} />
-        )}
       </div>
     );
   }
--- src/components/QuizKana.js
+++ src/components/QuizKana.js
@@ -1,16 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
+import styled from '@emotion/styled';
+import Settings from './Settings';
 import Quiz from './Quiz';
 
 // Character sets
 import hiragana from '../lib/hiragana';
 import katakana from '../lib/katakana';
 
+import { ReactComponent as SettingsIcon } from '../assets/round-settings-24px.svg';
+
+const SettingsLink = styled.button`
+  position: absolute;
+  bottom: 1em;
+  left: 1em;
+  background: none;
+  border: 0;
+  opacity: 0.5;
+  cursor: pointer;
+  transition: opacity 0.2s ease-out;
+
+  span {
+    display: none;
+    padding-left: 0.4em;
+    vertical-align: middle;
+  }
+
+  &:hover {
+    opacity: 1;
+
+    span {
+      display: inline-block;
+    }
+  }
+`;
+
 // TODO: customize what can be tested per user
+class QuizKana extends Component {
+  state = {
+    showSettings: false,
+  };
 
-const QuizKana = props => {
-  const kana = hiragana.concat(katakana).sort(() => 0.5 - Math.random());
+  render() {
+    const { showSettings } = this.state;
+    const kana = hiragana.concat(katakana).sort(() => 0.5 - Math.random());
 
-  return <Quiz questions={kana} />;
-};
+    return (
+      <div>
+        <Quiz questions={kana} />
+        <SettingsLink
+          type="button"
+          onClick={() => this.setState({ showSettings: true })}
+        >
+          <SettingsIcon />
+          <span>Settings</span>
+        </SettingsLink>
+        {showSettings && (
+          <Settings onClose={() => this.setState({ showSettings: false })} />
+        )}
+      </div>
+    );
+  }
+}
 
 export default QuizKana;
