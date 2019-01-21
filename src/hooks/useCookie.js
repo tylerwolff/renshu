8d27b9f205af779b278f33c65794d4d494257a4e
Tyler Wolff <tylerwolff@gmail.com>
2019-01-21 17:48:34 -0500

    add useCookie hook

---
+++ src/hooks/useCookie.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+const setCookie = (name, value, options) => {
+  const expires = new Date(Date.now() + options.days * 864e5).toUTCString();
+  document.cookie =
+    name +
+    '=' +
+    encodeURIComponent(value) +
+    '; expires=' +
+    expires +
+    '; path=' +
+    options.path;
+};
+
+const getCookie = name => {
+  return document.cookie.split('; ').reduce((r, v) => {
+    const parts = v.split('=');
+    return parts[0] === name ? decodeURIComponent(parts[1]) : r;
+  }, '');
+};
+
+export default (key, initialValue) => {
+  const [item, setItem] = useState(getCookie(key) || initialValue);
+
+  const updateItem = (value, options = { days: 7, path: '/' }) => {
+    setItem(value);
+    setCookie(key, value, options);
+  };
+
+  return [item, updateItem];
+};
