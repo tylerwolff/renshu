520a3c11cc37483baa6ae31d7eb71aba00df165e
Tyler Wolff <tylerwolff@gmail.com>
2019-01-27 13:41:36 -0500

    add adjective library

--- src/components/QuizAdjectives.js
+++ src/components/QuizAdjectives.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Quiz from './Quiz';
-import numbers from '../lib/numbers';
+import adjectives from '../lib/adjectives';
 
 const QuizAdjectives = props => {
-  const randomizedNumbers = numbers.sort(() => 0.5 - Math.random());
-  return <Quiz questions={randomizedNumbers} />;
+  const randomizedAdjectives = adjectives.sort(() => 0.5 - Math.random());
+  return <Quiz questions={randomizedAdjectives} />;
 };
 
 export default QuizAdjectives;
---
+++ src/lib/adjectives.js
@@ -0,0 +1,33 @@
+export default [
+  // i-adjectives
+  { character: 'おおきい', sound: 'big', romaji: 'ookii' },
+  { character: 'ちいさい', sound: 'small', romaji: 'chiisai' },
+  { character: 'いい', sound: 'good', romaji: 'ii' },
+  { character: 'わるい', sound: 'bad', romaji: 'warui' },
+  { character: 'すごい', sound: 'great', romaji: 'sugoi' },
+  { character: 'あたらしい', sound: 'new', romaji: 'atarashii' },
+  { character: 'ふるい', sound: 'old', romaji: 'furui' },
+  { character: 'さむい', sound: 'cold', romaji: 'samui' },
+  { character: 'さむい', sound: 'hot', romaji: 'atsui' },
+  { character: 'かっこいい', sound: 'cool', romaji: 'kakkoii' },
+  { character: 'たかい', sound: 'expensive', romaji: 'takai' },
+  { character: 'やすい', sound: 'cheap', romaji: 'yasui' },
+  { character: 'きたない', sound: 'dirty', romaji: 'kitanai' },
+  { character: 'たのしい', sound: 'fun', romaji: 'tanoshii' },
+  { character: 'つまらない', sound: 'boring', romaji: 'tsumaranai' },
+  { character: 'やさしい', sound: 'easy', romaji: 'yasashii' },
+  { character: 'むずかしい', sound: 'dificult', romaji: 'muzukashii' },
+  { character: 'おもしろい', sound: 'interesting', romaji: 'omoshiroi' },
+  { character: 'いそがしい', sound: 'busy', romaji: 'isogashii' },
+  { character: 'こわい', sound: 'scary', romaji: 'kowai' },
+
+  // na-adjectives
+  { character: 'きれい(な)', sound: 'pretty', romaji: 'kirei' },
+  { character: 'しずか(な)', sound: 'quiet', romaji: 'shizuka' },
+  { character: 'にぎやか(な)', sound: 'lively', romaji: 'nigiyaka' },
+  { character: 'げんき(な)', sound: 'happy', romaji: 'genki' },
+  { character: 'げんき(な)', sound: 'happy', romaji: 'genki' },
+  { character: 'すき(な)', sound: 'like', romaji: 'suki' },
+  { character: 'きらい(な)', sound: 'dislike', romaji: 'kirai' },
+  { character: 'ひま(な)', sound: 'free', romaji: 'hima' },
+];
